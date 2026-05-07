d379bfad7a67648ac0573bf80e51b447397e3da0
cristoredentor <96353429+cristoredentor@users.noreply.github.com>
2026-05-07 16:25:10 -0600

    Refactor raw.casoscovid2021 table structure and data types
    This script modifies the raw.casoscovid2021 table by dropping unnecessary columns, updating date formats, converting data types, and creating a new categorical type for specific columns.

---
+++ data/Limpieza1.sql
@@ -0,0 +1,83 @@
+ALTER TABLE raw.casoscovid2021 
+
+DROP COLUMN IF EXISTS dummy,                 
+DROP COLUMN IF EXISTS habla_lengua_indig,   
+DROP COLUMN IF EXISTS otro_caso, 
+DROP COLUMN IF EXISTS toma_muestra_lab,
+DROP COLUMN IF EXISTS toma_muestra_antigeno;
+
+
+--Cambiamos las fechas que dicen NA por NULL
+UPDATE raw.casoscovid2021 
+SET fecha_def = NULL 
+WHERE fecha_def = 'NA';
+
+--Buscamos si hay alguna fecha con formato invalido
+
+SELECT fecha_actualizacion, fecha_ingreso, fecha_sintomas, fecha_def
+FROM raw.casoscovid2021
+WHERE fecha_actualizacion !~ '^\d{4}-\d{2}-\d{2}$'
+   OR fecha_ingreso !~ '^\d{4}-\d{2}-\d{2}$'
+   OR fecha_sintomas !~ '^\d{4}-\d{2}-\d{2}$'
+   OR fecha_def !~ '^\d{4}-\d{2}-\d{2}$'
+LIMIT 10;
+
+
+
+START TRANSACTION
+
+-- 1. Convertir columnas de fecha
+ALTER TABLE raw.casoscovid2021 
+ALTER COLUMN fecha_actualizacion TYPE DATE USING fecha_actualizacion::DATE,
+ALTER COLUMN fecha_ingreso TYPE DATE USING fecha_ingreso::DATE,
+ALTER COLUMN fecha_sintomas TYPE DATE USING fecha_sintomas::DATE,
+ALTER COLUMN fecha_def TYPE DATE USING fecha_def::DATE;
+
+-- 2. Convertir edad a número
+ALTER TABLE raw.casoscovid2021 
+ALTER COLUMN edad TYPE INTEGER USING edad::INTEGER;
+
+COMMIT;
+
+-- Ahora veamos los distintos valores de las columnas que queremos usar como boolean
+
+CREATE TYPE estado_categorico AS ENUM ('SI', 'NO', 'SE IGNORA', 'NO ESPECIFICADO', 'NO APLICA');
+
+START TRANSACTION
+
+ALTER TABLE raw.casoscovid2021 
+ALTER COLUMN intubado TYPE estado_categorico USING intubado::estado_categorico,
+ALTER COLUMN neumonia TYPE estado_categorico USING neumonia::estado_categorico,
+ALTER COLUMN indigena TYPE estado_categorico USING indigena::estado_categorico,
+ALTER COLUMN diabetes TYPE estado_categorico USING diabetes::estado_categorico,
+ALTER COLUMN epoc TYPE estado_categorico USING epoc::estado_categorico,
+ALTER COLUMN asma TYPE estado_categorico USING asma::estado_categorico,
+ALTER COLUMN inmusupr TYPE estado_categorico USING inmusupr::estado_categorico,
+ALTER COLUMN hipertension TYPE estado_categorico USING hipertension::estado_categorico,
+ALTER COLUMN otra_com TYPE estado_categorico USING otra_com::estado_categorico,
+ALTER COLUMN cardiovascular TYPE estado_categorico USING cardiovascular::estado_categorico,
+ALTER COLUMN obesidad TYPE estado_categorico USING obesidad::estado_categorico,
+ALTER COLUMN renal_cronica TYPE estado_categorico USING renal_cronica::estado_categorico,
+ALTER COLUMN tabaquismo TYPE estado_categorico USING tabaquismo::estado_categorico,
+ALTER COLUMN embarazo TYPE estado_categorico USING embarazo::estado_categorico;
+
+COMMIT;
+
+START TRANSACTION;
+ALTER TABLE raw.casoscovid2021 
+ALTER COLUMN id_registro TYPE VARCHAR(50),
+ALTER COLUMN origen TYPE VARCHAR(100),
+ALTER COLUMN sector TYPE VARCHAR(100),
+ALTER COLUMN entidad_um TYPE VARCHAR(100),
+ALTER COLUMN sexo TYPE VARCHAR(10),
+ALTER COLUMN entidad_nac TYPE VARCHAR(100),
+ALTER COLUMN entidad_res TYPE VARCHAR(100),
+ALTER COLUMN municipio_res TYPE VARCHAR(100),
+ALTER COLUMN tipo_paciente TYPE VARCHAR(50),
+ALTER COLUMN nacionalidad TYPE VARCHAR(50),
+ALTER COLUMN resultado_lab TYPE VARCHAR(100),
+ALTER COLUMN resultado_antigeno TYPE VARCHAR(100),
+ALTER COLUMN clasificacion_final TYPE VARCHAR(100),
+ALTER COLUMN pais_nacionalidad TYPE VARCHAR(100),
+ALTER COLUMN pais_origen TYPE VARCHAR(100);
+COMMIT;
